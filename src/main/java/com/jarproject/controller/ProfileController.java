d312b93af74deb8285121b5c11ce3ab624e0cdf8
CanhVeTaiGia <lvy16012005@gmail.com>
2025-06-19 01:19:18 +0700

    finished profile page

--- src/main/java/com/jarproject/controller/ProfileController.java
+++ src/main/java/com/jarproject/controller/ProfileController.java
@@ -7,7 +7,6 @@ import com.jarproject.entity.Student;
 import com.jarproject.service.student.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
